efbe3c38c13152b309a136e6e7987c8cb32b06de
KelleyRicker <kelley.ricker@grapecity.com>
2015-10-06 15:47:37 -0400

    XuniStockCSV
    Adding XuniStockCSV sample.

---
+++ Eclipse/XuniStockCSV/README.md
@@ -0,0 +1,6 @@
+Xuni Stock CSV
+=====================
+
+This sample uses FlexGrid to visualize stock data imported from a CSV file. It demonstrates both reading and parsing a CSV file into a FlexGrid and exporting the changes back to a saved CSV.
+
+![](Screenshot.png)
---
+++ Eclipse/XuniStockCSV/XuniStockCSV/src/com/example/xunistockcsv/License.java
@@ -0,0 +1,62 @@
+package com.example.xunistockcsv;
+
+public class License
+{
+	 public static final String KEY = 
+	            "ACABIAIgB4hYAHUAbgBpAFMAdABvAGMAawBDAFMAVgB9yUcq/7ywMEFtCtOGhx5H" +
+	            "PFe+v9gcbbRQ+DLYuBqIjJOTvnys9K+zON4ljslJuZ4zAIHpc+gpN8mJEqz+oc9w" +
+	            "5zUo6QZ5Gqpts1Oeqp6LK1DtuWYzL+gerqO9yui1jpCTgF99e/TLOjVMJ35QWy5E" +
+	            "QbMeA2L+75Cst6p3wNFS8egkGEe403znuuFo68hV/DP3V8uvRDPIfbyEseoQzaAr" +
+	            "mTJpowQwB8sSKUh9qHuXcvbXXaz9VbiN0XjEFn+r+fIF01DxBP7jU7cbKM17UuOi" +
+	            "9matGmHUkFWnXrL6+Y1mvSXYhjOj4T+EBHEgu+6I5bUmZrg0MSRzsLaQYN0oUwpY" +
+	            "JVBmoN++/LUpdrft6tMuY8C830ox0LiRQMr2gSje/7EeD5bLxzVOlopRqGfTY08+" +
+	            "QnM5BESY8RQqv7oXdrnPt9gsuaNgsVmzE8qRPg5bkW6+KnQTMgs6MnYXMajXKh9X" +
+	            "p5gAhKMpPZjkr27DXs6A8y3jL8Rx7HtiZ9HZr1TheeEe9nwKjflFyXO7DD7tz6nM" +
+	            "yElZJG6IOG9rjRwvhlT3LPbPdNo6KHQvYhd+rboQE5sNCcFmlK3yVwMvJSzF2hIN" +
+	            "DrS8jcBjmTQscW+GhiehKdFiPBeRRtDU959BduemS3sVKt8LHKwCc2iDCEI2UrCb" +
+	            "9F2VwSMNIb6E3tATnJ0O+zCCBWQwggRMoAMCAQICECIQshdLCxJ/uygFLhGzJQow" +
+	            "DQYJKoZIhvcNAQEFBQAwgbQxCzAJBgNVBAYTAlVTMRcwFQYDVQQKEw5WZXJpU2ln" +
+	            "biwgSW5jLjEfMB0GA1UECxMWVmVyaVNpZ24gVHJ1c3QgTmV0d29yazE7MDkGA1UE" +
+	            "CxMyVGVybXMgb2YgdXNlIGF0IGh0dHBzOi8vd3d3LnZlcmlzaWduLmNvbS9ycGEg" +
+	            "KGMpMTAxLjAsBgNVBAMTJVZlcmlTaWduIENsYXNzIDMgQ29kZSBTaWduaW5nIDIw" +
+	            "MTAgQ0EwHhcNMTMwOTI0MDAwMDAwWhcNMTYxMDIzMjM1OTU5WjCBpzELMAkGA1UE" +
+	            "BhMCVVMxFTATBgNVBAgTDFBlbm5zeWx2YW5pYTETMBEGA1UEBxMKUGl0dHNidXJn" +
+	            "aDEVMBMGA1UEChQMQ29tcG9uZW50T25lMT4wPAYDVQQLEzVEaWdpdGFsIElEIENs" +
+	            "YXNzIDMgLSBNaWNyb3NvZnQgU29mdHdhcmUgVmFsaWRhdGlvbiB2MjEVMBMGA1UE" +
+	            "AxQMQ29tcG9uZW50T25lMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA" +
+	            "ucugmqlJVWqckvNrTmVMhxqRy/KXt3EHFn5zCTgKqTr5RoLp/lnku9EPX4CGIBG6" +
+	            "UiAju8+CQJ/5zzeI5WJBbIm5cUkycZq9rcBnpf+jQNpSrNjMU5bP8ysDM4m9gy2u" +
+	            "P81P2bwFZ6L5SRjU1ZTK2JJrQkg1i3nmL8XO3Fe0/srbsuPdljDBSQ0s4onh/6qR" +
+	            "HRBKfKBhRBDIwM4uDm99iQMt1RCQ2t60FPYlnrHp2Q1wKmJ/l1tnTUw4UQSNkmUZ" +
+	            "2e+t6e45h/DkI2WgNIJHO21Inz0m0k6NDHKsFB/XKU5eiJcI+nMBcJTZIX91hdKK" +
+	            "ZUzylPQ1nulQ0bUf4DPacwIDAQABo4IBezCCAXcwCQYDVR0TBAIwADAOBgNVHQ8B" +
+	            "Af8EBAMCB4AwQAYDVR0fBDkwNzA1oDOgMYYvaHR0cDovL2NzYzMtMjAxMC1jcmwu" +
+	            "dmVyaXNpZ24uY29tL0NTQzMtMjAxMC5jcmwwRAYDVR0gBD0wOzA5BgtghkgBhvhF" +
+	            "AQcXAzAqMCgGCCsGAQUFBwIBFhxodHRwczovL3d3dy52ZXJpc2lnbi5jb20vcnBh" +
+	            "MBMGA1UdJQQMMAoGCCsGAQUFBwMDMHEGCCsGAQUFBwEBBGUwYzAkBggrBgEFBQcw" +
+	            "AYYYaHR0cDovL29jc3AudmVyaXNpZ24uY29tMDsGCCsGAQUFBzAChi9odHRwOi8v" +
+	            "Y3NjMy0yMDEwLWFpYS52ZXJpc2lnbi5jb20vQ1NDMy0yMDEwLmNlcjAfBgNVHSME" +
+	            "GDAWgBTPmanqeyb0S8mOj9fwBSbv49KnnTARBglghkgBhvhCAQEEBAMCBBAwFgYK" +
+	            "KwYBBAGCNwIBGwQIMAYBAQABAf8wDQYJKoZIhvcNAQEFBQADggEBAGHNVjnOPBgA" +
+	            "WOYhrYlJZup5dDWOt/ajkOhFhaAj/7gsSkn5Taj5UAjmQhhI0anliOrte9CiyOz8" +
+	            "Lqp3Cl9N3duHaUQRHhcJHOmj7gcr1bHCPQPw/VShSfjcuOVswH8bNFGE2rQE34RO" +
+	            "UPT4F6OymhSyx3kZGEYs05ea0NX739ruPuH23kkQyT74luXKxV7pSlyC2hj1eC5k" +
+	            "uybkM6FBPRAiWA4grVBKX/CGIoZ08F8PmM3j9IewZVRs+kL4/GOHnJP8tKb342qT" +
+	            "63MxBByltN94sqz2QuCwbhSj1+yDnA6XgU3gIEejYItcSq2uLLf/+ulQw661Uqab" +
+	            "VrAeGAaqy4UwggK4MIIBoKADAgECAghQYqIgG6pzBzANBgkqhkiG9w0BAQUFADAc" +
+	            "MRowGAYDVQQDDBFHQy1YVTExNTAwLTE0MDAyMjAeFw0xNTAxMDEwMDAwMDBaFw0x" +
+	            "NjA0MzAwMDAwMDBaMBwxGjAYBgNVBAMMEUdDLVhVMTE1MDAtMTQwMDIyMIIBIjAN" +
+	            "BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsqhBxphConGf6hbxWUXpWexaJ8j1" +
+	            "6mAVd6p1l/oAaqREq/luxI4pfEvI7ue+bNnDksYx8x4rS6ibuqjtkbrbRw8i0nc4" +
+	            "lDqpKX/n55tjLiYQgTZQV+FUiVLFxrUxVR6czDA/njva+4Tn2Gba/blBSVDxd1pm" +
+	            "3VNhEMNJnXtO/BPOofZ24L3KFgANR2XpaCulbAmx1nxHMl6vekKlzw27Ao9RYLUn" +
+	            "tE7dh3TIwMZXkORe3stUeHL4U78jjAOc03b+kcMwp5RgV5J0lX4n3PkGLfPGDQdE" +
+	            "xahxXuvspzDZVd3wYQnIgH5lNzOHJm1YqpMQRdQpOnV/2x+S/qfNAsvyQQIDAQAB" +
+	            "MA0GCSqGSIb3DQEBBQUAA4IBAQCV6fzoYNiVdtRs/he3NNfet/1WHcCIWJW3rzR/" +
+	            "fPVTob3C3gXV2Q4JP3TbghIhnP59A+M59pBk4UcET2YGmVA5uftXVQGtJU/+U68T" +
+	            "QjTqb1FFFVnGYU9FujyP8kO9OvutusH8tkmLs9WCLzrUM9za5NCieqfdqOwU8M0b" +
+	            "iKbU2LWrxNUiIgT4VLCTW8eCE2jwT9hsY9QPpVEZBSk5o3dZn0uU+LBI104cw54P" +
+	            "R9I7YRf2oHdbO3K2OPeB+hHBt/Nzp7g4+DS5PG3uqESSoa/vxUDBRVFghOqdHuVE" +
+	            "pGihN2wqzEPiA1OtjZy1s80/KjOPqWOTw03kOQH4TsyLlm78";
+}
+
